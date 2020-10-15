4aa44dbca6a829e648d277ca3af8d9360561e7f3
IainLR <iainrobertzon@gmail.com>
2020-10-15 16:23:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2)
+}
+
+// don't use (), that invokes, while we just want to store
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => {
+    return function(fare){
+        return fare * int
+    }
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare)
+
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare)
+
+const selectDifferentDrivers = (drivers, func) => {
+    return func(drivers)
+}
